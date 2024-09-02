18d5764a3bcd8d288fad8c5c93770b792efe7a12
Joao Felipe <j155c137@gmail.com>
2024-09-02 14:20:43 -0300

    Primeiro commit
    Criando o projeto Java Spring

---
+++ src/test/java/com/joao/CRUD_Simples/CrudSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.CRUD_Simples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
