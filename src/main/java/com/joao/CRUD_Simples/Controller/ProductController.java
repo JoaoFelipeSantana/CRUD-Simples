396666d0383b2f886a60ff376b84e1be298dc571
Joao Felipe <j155c137@gmail.com>
2024-09-02 15:17:57 -0300

    [feat] - Criando o controller resposável pelo endpoint '/produtos'

---
+++ src/main/java/com/joao/CRUD_Simples/Controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.joao.CRUD_Simples.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProductController {
+
+
+}
