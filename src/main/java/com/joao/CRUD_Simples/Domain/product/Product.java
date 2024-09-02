77ba1c278ba2355cec078a898b0db767325cfe7b
Joao Felipe <j155c137@gmail.com>
2024-09-02 15:11:25 -0300

    [feat] - Criando a entidade 'product'

---
+++ src/main/java/com/joao/CRUD_Simples/Domain/product/Product.java
@@ -0,0 +1,26 @@
+package com.joao.CRUD_Simples.Domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Table(name="product")
+@Entity(name="product")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String supplier;
+    private Date dt_validity;
+    private Date dt_manufacture;
+    private boolean active;
+
+}
