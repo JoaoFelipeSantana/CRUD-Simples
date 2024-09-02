3e17533cc3bec2daf0b1a447fe80f98d4fe35a7e
Joao Felipe <j155c137@gmail.com>
2024-09-02 16:23:16 -0300

    [feat] - Alterando o tipo dos dados dos campos referente a datas

--- src/main/java/com/joao/CRUD_Simples/Domain/product/ProductDTO.java
+++ src/main/java/com/joao/CRUD_Simples/Domain/product/ProductDTO.java
@@ -1,8 +1,16 @@
 package com.joao.CRUD_Simples.Domain.product;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
-public record ProductDTO(String name, String supplier, Date dt_validity, Date dt_manufacture, int amount) {
+public record ProductDTO(String name,
+                         String supplier,
+
+                         String dt_validity,
+
+                         String dt_manufacture,
+                         int amount) {
 
 }
