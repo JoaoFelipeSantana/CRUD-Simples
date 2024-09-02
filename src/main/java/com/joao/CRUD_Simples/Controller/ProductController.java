6e220da82abbcc29f1ca386a720e0966adfaea0c
Joao Felipe <j155c137@gmail.com>
2024-09-02 15:22:29 -0300

    [feat] - Criando o método 'GET' da API

--- src/main/java/com/joao/CRUD_Simples/Controller/ProductController.java
+++ src/main/java/com/joao/CRUD_Simples/Controller/ProductController.java
@@ -1,5 +1,9 @@
 package com.joao.CRUD_Simples.Controller;
 
+import com.joao.CRUD_Simples.Domain.product.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/produtos")
 public class ProductController {
 
+    @Autowired ProductRepository repository;
 
+    @GetMapping
+    public ResponseEntity getAllProduct() {
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
 }
